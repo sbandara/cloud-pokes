56917e1ed907e4f45fdd42544263a308421a89af
Samuel Bandara <Samuel_Bandara@hms.harvard.edu>
2015-04-25 20:18:46 -0400

    Implement asynchronous rewind.

--- src/com/sbandara/cloudpokes/NotificationBuffer.java
+++ src/com/sbandara/cloudpokes/NotificationBuffer.java
@@ -3,33 +3,154 @@ package com.sbandara.cloudpokes;
 public class NotificationBuffer {
 	
 	private final static int TAPE_LENGTH = 128, MIN_RETAIN_MILLIS = 1000;
-
-	private class Entry {
+	
+	private final class Entry {
 		
-		Entry(ApnsNotification notification, long queued) {
+		Entry(ApnsNotification notification, int id) {
 			this.notification = notification;
-			this.queued = queued;
+			this.id = id;
 		}
 		
 		final ApnsNotification notification;
-		final long queued;
+		final int id;
+		long sent = 0;
+		
+		void send() {
+			try {
+				notification.send();
+			}
+			catch (RuntimeException e) {
+				System.out.println("Unhandled Exception.");
+			}
+			sent = System.currentTimeMillis();
+		}
 	}
 	
 	private final Entry[] tape = new Entry[TAPE_LENGTH];
+	private int tail = 0, head = 0;
+	private final Consumer consumer = new Consumer();
+	private volatile Thread worker = null;
+	private volatile boolean is_rewinding = false;
 	
-	private int mem = 0, out = 0, head = 0;
-	
-	synchronized void enqueue(ApnsNotification notification) {
-		final long now = System.currentTimeMillis();
-		Entry entry = new Entry(notification, now);
+	synchronized public void enqueue(ApnsNotification notification, int id) {
+		boolean was_interrupted = false;
 		if (++ head == TAPE_LENGTH) {
 			head = 0;
 		}
-		if (head == mem) {
-			long elapsed = now - tape[mem].queued;
-			if (elapsed < MIN_RETAIN_MILLIS) {
-				
+		if (tape[head] != null) {
+			synchronized (tape) {
+				while (head == tail) {
+					try {
+						tape.wait();
+					}
+					catch (InterruptedException e) {
+						was_interrupted = true;
+					}
+				}
 			}
+			for (;;) {
+				long elapsed = System.currentTimeMillis() - tape[head].sent;
+				if (elapsed < MIN_RETAIN_MILLIS) {
+					break;
+				}
+				try {
+					Thread.sleep(MIN_RETAIN_MILLIS - elapsed + 1);
+				}
+				catch (InterruptedException e) {
+					was_interrupted = true;
+				}
+			}
+		}
+		synchronized (consumer) {
+			tape[head] = new Entry(notification, id);
+			if ((worker == null) && (! is_rewinding)) {
+				worker = new Thread(consumer);
+				worker.start();
+			}
+		}
+		if (was_interrupted) {
+			Thread.currentThread().interrupt();
+		}
+	}
+	
+	private final class Consumer implements Runnable {
+		
+		public void run() {
+			if (! hasJobs()) {
+				return;
+			}
+			for (;;) {						
+				tape[tail].send();
+				synchronized (tape) {
+					if (++ tail == TAPE_LENGTH) {
+						tail = 0;
+					}
+					tape.notify();
+				}
+				synchronized (consumer) {
+					if ((Thread.interrupted()) || (! hasJobs())) {
+						worker = null;
+						consumer.notify();
+						return;
+					}
+				}
+			}
+		}
+		
+		private boolean hasJobs() {
+			return (tape[tail] != null) && (tape[tail].sent == 0);
+		}
+	}
+	
+	private final int findEntry(int id) {
+		for (int k = 0; k < TAPE_LENGTH; k ++) {
+			if (tape[k].id == id) {
+				return k;
+			}
+		}
+		return -1;
+	}
+	
+	public final void rewind(int id) throws EntryNotFoundException {
+		boolean was_interrupted = false;
+		synchronized (consumer) {
+			if (worker != null) {
+				worker.interrupt();
+				while (worker != null) {
+					try {
+						consumer.wait();
+					}
+					catch (InterruptedException e) {
+						was_interrupted = true;
+					}
+				}
+			}
+			is_rewinding = true;
+		}
+		final int idx = findEntry(id);
+		tail = idx;
+		for (int k = tail; k != head; k ++) {
+			if (k == TAPE_LENGTH) {
+				k = 0;
+			}
+			if (tape[k] == null) {
+				continue;
+			}
+			if (tape[k].sent == 0) {
+				break;
+			}
+			tape[k].sent = 0;
+		}
+		if (consumer.hasJobs()) {
+			worker = new Thread(consumer);
+			worker.start();
+		}
+		is_rewinding = false;
+		if (was_interrupted) {
+			Thread.currentThread().interrupt();
+		}
+		if (idx == -1) {
+			throw new EntryNotFoundException(id);
 		}
 	}
 }
